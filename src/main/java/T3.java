718596690968d387be8c7d0e5e61e9bd482303a0
edwardsues <edward.su29@gmail.com>
2021-09-27 19:46:44 -0400

    Updated javadocs.

--- src/main/java/T3.java
+++ src/main/java/T3.java
@@ -4,27 +4,45 @@
 public class T3 implements Drivable, Tradable{
     private int s;
 
+    /**
+     * Construct a T3, giving it a speed value of 100.
+     */
     public T3(){
         this.s = 100;
     }
 
+
+    /**
+     * Add 5 to the speed instance variable.
+     */
     @Override
     public void upgradeSpeed() {
         this.s += 5;
 
     }
 
+    /**
+     * Decrease the speed variable by 1
+     */
     @Override
     public void downgradeSpeed() {
         this.s--;
 
     }
 
+    /**
+     * Return the max speed value.
+     * @return the speed value
+     */
     @Override
     public int getMaxSpeed() {
         return s;
     }
 
+    /**
+     * Return the price of the vehicle.
+     * @return the price of the Tesla Model 3
+     */
     @Override
     public int getPrice() {
         return 45000;
