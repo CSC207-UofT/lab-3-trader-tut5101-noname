fda9c75c85a3bf70ed443bbcb184d9e332f91546
glyon3 <georgewesley17@gmail.com>
2021-09-27 20:04:47 -0400

    Wolf

--- src/main/java/Wolf.java
+++ src/main/java/Wolf.java
@@ -1,32 +1,45 @@
 public class Wolf implements Drivable, Domesticatable, Tradable{
-    private int maxSpeed =31; //average land speed of wolf in miles per hour
+    private int maxSpeed;
+
+    /** Creates a wolf with a max speed of 31
+     *
+     */
+    public Wolf() {
+        this.maxSpeed = 31;
+    }
+    /** Increases speed by 1
+     *
+     */
     @Override
     public void upgradeSpeed() {
-        //Is your wolf a better wolf than the average wolf? Then this method is for you!
         this.maxSpeed += 1;
     }
-
+    /** Decreases speed by 1
+     *
+     */
     @Override
     public void downgradeSpeed() {
-        //Is your wolf a little slow? Maybe they just don't like running? Well in that case, downgrade your wolf today!
         this.maxSpeed -= 1;
     }
-
+    /** Returns the speed of the care
+     * @return The speed of the car
+     */
     @Override
     public int getMaxSpeed() {
-        //Need to know your wolf's top speed? Well here you go!
         return this.maxSpeed;
     }
-
+    /** Returns the sound a wolf makes
+     * @return The sound a wolf makes
+     */
     @Override
     public String sound() {
-        //A wolf makes this sound
         return "GRRRRRR";
     }
-
+    /** Returns the price of the wolf relative to its speed
+     * @return The price of the wolf
+     */
     @Override
     public int getPrice() {
         return this.maxSpeed * 100;
-        //Wolves are worth their speed multiplied by 100. It is objective science don't ask me why
     }
 }
